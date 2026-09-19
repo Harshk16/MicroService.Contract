54a98df9d3d2ffc9849f1dbb75415136e8b2da10
Harsh <Harsh@gmail.com>
2026-09-19 13:59:10 +0530

    Initial contracts project

---
+++ OrderEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace MicroService.Contracts.Events;
+
+public class OrderCreatedEvent
+{
+    public Guid OrderId { get; set; }
+    public string CustomerId { get; set; } = "";
+    public decimal TotalAmount { get; set; }
+}
